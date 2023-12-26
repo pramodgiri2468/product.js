49cd8905fd764936b83f4d9e228b891a2ff64bc0
Pramod <giripramodsunway789@gmail.com>
2023-12-26 08:19:06 +0545

    Committed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import { ProductsTable } from './ProductTable';
+import { ProductsTable } from './ProductTable.js';
 
 const products = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-  ];
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
+  { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
+  { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
+  { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
+  { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
+];
 
 function App() {
   const [search, setSearch] = useState('');
-  const [inStockOnly, setInStockOnly] =useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const [inStockOnly, setInStockOnly] = useState(false);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
-  const toggleInStock = (e) =>{
+
+  const toggleInStock = (e) => {
     setInStockOnly(e.target.checked);
-  }
+  };
+
+  const selectCategory = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const filteredProducts = products
+    .filter((product) => {
+      if (selectedCategory === '') {
+        return true;
+      }
+      return product.category === selectedCategory;
+    })
+    .filter((product) => {
+      return product.name.toLowerCase().includes(search.toLowerCase());
+    })
+
+    .filter((product) => {
+      if (inStockOnly) {
+        return product.stocked;
+      }
+      return true;
+    });
+    
 
   return (
     <div className='app'>
-      <input placeholder='Search ...' onChange={handleChange} value={search}></input>
-      <div>
-        <input type='checkbox' onChange={toggleInStock} checked={inStockOnly} id='checkbox'></input>
-        <label htmlFor='checkbox'>Show only products in stock</label>
+      <input onChange={handleChange} value={search} placeholder='Search ...' />
+      <div className='flex justify-between'>
+        <div>
+          <input
+            type='checkbox'
+            onChange={toggleInStock}
+            checked={inStockOnly}
+            id='checkbox'
+          />
+          <label htmlFor='checkbox'>Show only products in stock</label>
+        </div>
+        <div>
+          <select value={selectedCategory} onChange={selectCategory}>
+            <option value=''>All</option>
+            <option value='Fruits'>Fruits</option>
+            <option value='Vegetables'>Vegetables</option>
+          </select>
+        </div>
       </div>
-      <ProductsTable category='Fruits' products={products} />
+      <ProductsTable category={selectedCategory} products={filteredProducts} />
     </div>
   );
 }
